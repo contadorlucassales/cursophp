21499cc74ccba11927c25a3e7cc003d49d9b75c8
contadorlucassales <srlucassales@gmail.com>
2023-09-16 17:05:04 -0300

    23-09-16

--- ex002/index.php
+++ ex002/index.php
@@ -9,11 +9,58 @@
 
 <body>
   <h1>Exemplo de PHP</h1>
-  <?php
-  date_default_timezone_set("America/Sao_Paulo"); //GMT-3
-  echo "Hoje é dia " . date("d/M/Y");
-  echo " e a hora atual é " . date("G:i:s T");
+
+  <p>
+    <?php 
+  $canal = "Curso em Vídeo";
+  $ano = date('Y');
+  echo <<< TESTE
+      Olá galera do $canal!
+            Tudo bem com vocês?
+        Como está sendo este ano de $ano?
+    Abraços! \u{1F596}
+  TESTE;
+  
+  //Visualizável apenas em Textarea
   ?>
+  </p>
+
+  <p>
+    <?php 
+  $nom = "Rodrigo";
+  $snom = "Nogueira";
+  echo "$nom \"Minotauro\" $snom";
+  ?>
+  </p>
+
+  <p>
+    <?php 
+  const ESTADO = "MG";
+  echo "Moro em " . ESTADO . ".";
+  ?>
+  </p>
+
+  <p>
+    <?php 
+  $nome = "Lucas";
+  $sobrenome = "Sales";
+  echo '$nome $sobrenome';
+  ?>
+  </p>
+
+  <p>
+    <?php 
+  $nome = "Lucas";
+  $sobrenome = "Sales";
+  echo "$nome $sobrenome";
+  ?>
+  </p>
+
+  <?php  
+  date_default_timezone_set("America/Sao_Paulo"); //GMT-3
+  echo "Hoje é dia " . date("d/M/Y") . " e a hora atual é " . date("G:i:s T") . ".";
+  ?>
+
 </body>
 
 </html>
