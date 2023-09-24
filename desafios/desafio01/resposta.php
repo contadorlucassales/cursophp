e7608018263ee8084eb12dd5a2137abee0aa538e
contadorlucassales <srlucassales@gmail.com>
2023-09-24 19:38:18 -0300

    23-09-24

--- desafios/desafio01/resposta.php
+++ desafios/desafio01/resposta.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Resultado</title>
+  <title>Antecessor e Sucessor</title>
   <link rel="stylesheet" href="style.css">
 </head>
 
@@ -14,12 +14,12 @@
   </header>
   <main>
     <?php
-    $numero = $_GET["numero"] ?? "NaN";
-    $antecessor = $numero - 1;
-    $sucessor = $numero + 1;
-    echo "O número escolhido foi <strong>$numero</strong>.<br>
-    O seu <i>antecessor</i> é $antecessor.<br>
-    O seu <i>sucessor</i> é $sucessor.";
+      $numero = $_GET["numero"] ?? "NaN";
+      // $antecessor = $numero - 1;
+      // $sucessor = $numero + 1;
+      echo "O número escolhido foi <strong>$numero</strong>.\n";
+      echo "O seu <i>antecessor</i> é " . ($numero - 1) . ".\n";
+      echo "O seu <i>sucessor</i> é " . ($numero + 1) . ".";
     ?>
     <p><a href="javascript:history.go(-1)">Voltar</a></p>
 
